57330d43c0dedb9ebefcffdc4f073de45b35dce1
renatoexpert <renatorraraujo@gmail.com>
2021-11-22 01:54:44 -0300

    now its a <textarea> with pretty much more controls, and pointer to write and erase chars

--- bin/websh.js
+++ bin/websh.js
@@ -1,58 +1,106 @@
 // bash-like terminal for web
 
 //rendering initial screen
-document.body.innerHTML += '<div id="websh"></div>'
+//styles
+document.body.innerHTML += '<textarea type="text" id="websh" readonly></textarea>'
 webshdiv = document.getElementById('websh')
-webshdiv.style.minWidth = '160px'
-webshdiv.style.minHeight = '160px'
+webshdiv.style.minWidth = '400px'
+webshdiv.style.minHeight = '300px'
 webshdiv.style.backgroundColor = 'black'
 webshdiv.style.color = 'white'
-busy = false
-charblack = '▮'
-charwhite = '▯'
 
+//dataflow
+stdout_perm = ''
+stdin_cache = ''
+pointer_revpos = 0
+function pointer_pos () { return stdin_cache.length - pointer_revpos -1}
 function core () { return webshdiv.innerHTML }
 
+function update() {
+	webshdiv.innerHTML = stdout_perm + stdin_cache
+}
+
 function stdout (content) {
-	webshdiv.innerHTML += content
-}
-
-function replace_char (a,b) {
-	let text = webshdiv.innerHTML
-	let newt = text.replaceAll(a,b)
-	webshdiv.innerHTML = newt
-}
-
-async function penpointer() {
-	stdout (charwhite)
-	let pointer_pos = 0
-	let interval = 500
-	while (true) {
-		replace_char(charblack, charwhite)
-		await new Promise(r => setTimeout(r, interval));
-		replace_char(charwhite, charblack)
-		await new Promise(r => setTimeout(r, interval));
-		console.log(core()[ core().length - 1 - pointer_pos ])
-	}
+	stdout_perm += content
 }
 
 function readmode() {
-	let oldhtml = webshdiv.innterHTML
-	let stdin
+	update()
 	document.addEventListener('keydown', (k) => {
-		console.log('a')
-		stdout(k.key)
+		switch (k.key) {
+			case 'Enter':
+				submitsh(stdin_cache)
+				stdin_cache = ''
+				break
+			case 'Backspace':
+				deletechar(0)
+				break
+			case 'Delete' :
+				deletechar(1)
+				break
+			case 'ArrowLeft':
+				if (pointer_revpos < stdin_cache.length) {
+					pointer_revpos++ 
+				}
+				break
+			case 'ArrowRight':
+				if (pointer_revpos > 0) { 
+					pointer_revpos-- 
+				}
+				break
+			case 'Escape':
+				console.log('you pressioned escape')
+				break
+			case 'Alt':
+				break
+			case 'AltGraph':
+				break
+			case 'Control':
+				break
+			case 'Shift':
+				break
+			case 'CapsLock':
+				break
+			default:
+				addchar(k.key)
+				update()
+				return
+		}
+		update()
 	})
 }
 
-function br () {
-	stdout('<br>')
+function addchar(arg) {
+	let a = stdin_cache.slice(0, pointer_pos()+1)
+	let b = stdin_cache.slice(pointer_pos()+1, stdin_cache.length)
+	stdin_cache = a + arg + b
+}
+
+function deletechar(isDel = 0) {
+	let a = stdin_cache.substring(0, pointer_pos() + isDel)
+	let b = stdin_cache.substring(pointer_pos() + 1 + isDel, stdin_cache.length)
+	stdin_cache = a + b
+	pointer_revpos -= isDel
+}
+
+function breakline () {
+	stdout('\n')
+	pointer_revpos = 0
+}
+
+function printl (content) {
+	stdout(content)
+	breakline()
+}
+
+function submitsh (content) {
+	printl(content)
+	salute()
 }
 
 function salute () {
 	stdout('root@zoso:~#')
-	readmode();
 }
 
 salute()
-penpointer()
+readmode()
