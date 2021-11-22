46b7a5707e8f6d6c5f07fb8f8476179ccf392b5d
renatoexpert <renatorraraujo@gmail.com>
2021-11-22 16:20:04 -0300

    now prompting boot steps using screen driver

---
+++ boot/init.js
@@ -0,0 +1,37 @@
+// Boot the machine
+
+// step1. get configurations from a especific file
+// step2. if no config is storaged, get the template config
+// step3. prompt login shell
+
+import { shellscreen } from '/lib/modules/egg/kernel/drivers/prompt_driver.js'
+
+boot()
+
+function boot() {
+	var display = shellscreen
+	display.create()
+	display.stdout ('Zoso Kernel version="Egg"\n')
+	display.stdout ('\n'+
+		'▒███████▒ ▒█████    ██████  ▒█████  \n'+
+		'▒ ▒ ▒ ▄▀░▒██▒  ██▒▒██    ▒ ▒██▒  ██▒\n'+
+		'░ ▒ ▄▀▒░ ▒██░  ██▒░ ▓██▄   ▒██░  ██▒\n'+
+		'  ▄▀▒   ░▒██   ██░  ▒   ██▒▒██   ██░\n'+
+		'▒███████▒░ ████▓▒░▒██████▒▒░ ████▓▒░\n'+
+		'░▒▒ ▓░▒░▒░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░░ ▒░▒░▒░ \n'+
+		'░░▒ ▒ ░ ▒  ░ ▒ ▒░ ░ ░▒  ░ ░  ░ ▒ ▒░ \n'+
+		'░ ░ ░ ░ ░░ ░ ░ ▒  ░  ░  ░  ░ ░ ░ ▒  \n'+
+		'  ░ ░        ░ ░        ░      ░ ░  \n'+
+		'░                                   \n\n'
+	)
+	display.stdout ('Initializing...\n')
+	display.stdout ("[OK]	Screen drivers: resolution_detected=("
+		+screen.width
+		+"x"
+		+screen.height
+		+")\n"
+	)
+	// start memory driver
+	display.stdout ("Starting memory driver...")
+
+}
